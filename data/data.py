6625c7ce75993123f10fa560e4cdc623704bd604
andreev_af <andreev.alef@gmail.com>
2017-04-24 15:13:56 +0700

    Добавлен тестовый модуль данных data.py

--- data/data.py
+++ data/data.py
@@ -3,19 +3,32 @@ from __future__ import division
 from app.calc import rub_comma, okrug2
 
 # Типографские расходы
-white_paper_batch_cost_65 = 80
-white_sheets_per_batch_65 = 250
+white_paper_batch_cost_65 = 80  # Цена пачки белой бумаги 65г/м2
+white_sheets_per_batch_65 = 250  # Число листов в пачке 65г/м2
 
-color_paper_batch_cost = 650
-color_sheets_per_batch = 250
+color_paper_batch_cost = 650  # Цена пачки цветной бумаги
+color_sheets_per_batch = 250  # Число листов в пачке цветной бумаги
 
-pages_count = 100
+pages_count = 101  # Всего страиц
 
-while pages_count % 4:
-    pages_count += 1
+pages_count_tetra = pages_count  # Число страниц кратных 4-м
+while pages_count_tetra % 4:
+    pages_count_tetra += 1
 
-layout_sheets = pages_count // 4
+layout_sheets = pages_count_tetra // 4  # Количество тетрадей
+if layout_sheets >= 35:
+    if layout_sheets % 8:
+        brochure_count = layout_sheets // 8 + 1  # Количество брошюр при термоклеевом переплёте
+    else:
+        brochure_count = layout_sheets // 8
+else:
+    brochure_count = 1
 
-print rub_comma(45.37)
-print pages_count
-print layout_sheets
+number_of_copies = 100  # Тираж
+defect = 3.0  # Брак в %
+
+# ---------------------------------------------------------------------------------------------------------------------
+print "Страниц всего: {0}".format(pages_count)
+print "Страниц приведённых к 4-м: {0}".format(pages_count_tetra)
+print "Листов макета: {0}".format(layout_sheets)
+print "Число брошюр: {0}".format(brochure_count)
